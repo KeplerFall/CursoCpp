203dcca46bf3b19e092686c7a3987c9d4c2457e9
BuildTools <unconfigured@null.spigotmc.org>
2023-02-23 15:01:22 -0300

    First commit

---
+++ adv.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main (){
+    cout << "Bem vindo ao jogo de adivinhação" << endl;
+    cout << "Sexo";
+
+    int numero_secreto = 42;
+
+    cout << "O numero secreto é: " << numero_secreto << " nao conte a ninguem";
+}
